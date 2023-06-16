17f05fee7055e5abd768c0a24b7ab9082d97ba45
HugoWinterGhost <hfief1806@gmail.com>
2023-06-16 12:55:59 +0200

    advancing description

--- pages/Description.py
+++ pages/Description.py
@@ -10,14 +10,16 @@ def load_Infos(subtitle, description):
   st.text(description)
 
 titles = [
+  'Type de projet',
   'Technos utilisés',
   'Source de donnée utilisé',
   'Objectifs',
 ]
 description = [
-  'Python, Apprentissage non supervisé, Clustering',
-  'Kaggle (https://www.kaggle.com/datasets/killbot/linkedin-profiles-and-jobs-data)',
-  'Le but du projet était de donner des recommendations de postes à pourvoir, suivant des informations rentrées par un utilisateur',
+  'Système de recommandation d\'offres d\'emplois en fonction de profils Linkedin',
+  'Langage Python',
+  'Kaggle : https://www.kaggle.com/datasets/killbot/linkedin-profiles-and-jobs-data (pas besoin de scrapping)',
+  'Le but du projet était de donner des recommendations de postes à pourvoir, pour certains profils Linkedin',
 ]
 
 for x in range(len(titles)):
